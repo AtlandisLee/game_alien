95be91dad4c4486be803b81e152654997a853cfe
AtlandissLee <1433693288@qq.com>
2021-08-17 17:53:07 +0800

    增加难度等级显示

--- scoreboard.py
+++ scoreboard.py
@@ -17,8 +17,10 @@ class Scoreboard:
         self.highest_score_color = (255, 0, 0)
         self.font = pygame.font.SysFont(None, 48)
 
+        self.prep_level()
         self.prep_score()
         self.prep_highest_score()
+
 
     def prep_score(self):
         self.score_str = "{:,}".format(self.stats.score)
@@ -27,13 +29,13 @@ class Scoreboard:
 
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
+        self.score_rect.top = self.level_rect.bottom + 10
 
     def prep_highest_score(self):
         self.highest_score_str = "{:,}".format(self.stats.highest_score)
-        self.highest_score_image = self.font.render(self.highest_score_str,
-                                                    True, self.highest_score_color,
-                                                    self.settings.bg_color)
+        self.highest_score_image = self.font.render(
+            self.highest_score_str, True, self.highest_score_color,
+            self.settings.bg_color)
         self.highest_score_rect = self.highest_score_image.get_rect()
         self.highest_score_rect.centerx = self.screen_rect.centerx
         self.highest_score_rect.top = 20
@@ -43,6 +45,15 @@ class Scoreboard:
             self.stats.highest_score = self.stats.score
         self.prep_highest_score()
 
+    def prep_level(self):
+        self.level_str = f"LEVEL:{self.stats.level}"
+        self.level_image = self.font.render(
+            self.level_str, True, self.score_color, self.settings.bg_color)
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.screen_rect.right - 20
+        self.level_rect.top = 20
+
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.highest_score_image, self.highest_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
